0d1bc65af48e0fc74206a66fd5361d47067098f9
Louis Jannero Baclao <ljannero@pm.me>
2024-11-18 10:41:58 +0800

    Created Admin models

--- api/psgc/admin.py
+++ api/psgc/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from api.psgc.models import Region, Province, City, Barangay
 
-# Register your models here.
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name')
+    search_fields = ('code', 'name')
+
+
+@admin.register(Province)
+class ProvinceAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'region')
+    search_fields = ('code', 'name', 'region__name')
+    autocomplete_fields = ['region']
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'province', 'region')
+    search_fields = ('code', 'name', 'province__name', 'region__name')
+
+    # Enabling autocomplete_fields disables `smart_selects` function
+    # autocomplete_fields = ['province', 'region']
+
+
+@admin.register(Barangay)
+class BarangayAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'city', 'province', 'region')
+    search_fields = ('code', 'name', 'city__name', 'province__name', 'region__name')
+
+    # Enabling autocomplete_fields disables `smart_selects` function
+    # autocomplete_fields = ['city', 'province', 'region']
